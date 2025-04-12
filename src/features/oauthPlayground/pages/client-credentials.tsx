318fd6442c0134a3bf466a4bc3f72e51bd23e0e3
r-cz <racruz7@me.com>
2025-04-12 09:32:20 -0500

    fix navigation

--- src/features/oauthPlayground/pages/client-credentials.tsx
+++ src/features/oauthPlayground/pages/client-credentials.tsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
 import { PageContainer, PageHeader } from "@/components/page";
-import { OAuthFlowType } from "../utils/types";
-import { FlowSelector } from "../components/FlowSelector";
+import { ClientCredentialsFlow } from "../components/ClientCredentialsFlow";
 
 export default function ClientCredentialsPage() {
-  const [selectedFlow, setSelectedFlow] = useState<OAuthFlowType>(OAuthFlowType.CLIENT_CREDENTIALS);
-
   return (
     <PageContainer>
       <PageHeader
         title="OAuth 2.0 Playground"
         description="Test and explore the Client Credentials flow interactively."
       />
-      <FlowSelector selectedFlow={selectedFlow} onSelectFlow={setSelectedFlow} />
+      <ClientCredentialsFlow />
     </PageContainer>
   );
 }
